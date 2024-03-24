7ff7cd73bf255c887d829c9c365d8d3aea74241f
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-24 19:50:05 +0100

    FEAT -> Created the loginData hook for the login view and linked it with the cInputs inside

--- src/common/c-input/cInput.jsx
+++ src/common/c-input/cInput.jsx
@@ -1,13 +1,14 @@
 import './cInput.css'
 
-export const CInput = ({className,type,inputName,value, placeholder}) => {
+export const CInput = ({className,type,name,value, placeholder, onChange}) => {
     return (
         <input
             className={className}
             type={type}
-            name={inputName}
+            name={name}
             value={value}
             placeholder={placeholder}
+            onChange={onChange}
         />
     )
 }
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+
 import { CInput } from '../../common/c-input/cInput'
 import { Header } from '../../common/header/header'
 import './login.css'
 
 export const Login = () => {
+
+    const [loginData, setLoginData] = useState({
+        email: "",
+        passwordHash: ""
+    })
+
+    const inputHandler = (e) => {
+        console.log(e.target.name);
+        setLoginData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+
     return (
         <>
             <Header />
@@ -10,9 +27,18 @@ export const Login = () => {
                 <CInput
                     className={"inputDesign"}
                     type={"text"}
-                    name={"name"}
-                    value={""}
-                    placeholder={"input name"}
+                    name={"email"}
+                    value={loginData.email || ""}
+                    placeholder={"input email"}
+                    onChange={(e) => inputHandler(e)}
+                />
+                <CInput
+                    className={"inputDesign"}
+                    type={"password"}
+                    name={"passwordHash"}
+                    value={loginData.passwordHash || ""}
+                    placeholder={"input password"}
+                    onChange={(e) => inputHandler(e)}
                 />
             </div>
         </>
