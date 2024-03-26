e96511dca8a7bb5f272170ed25db9ad779df5892
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-26 15:20:46 +0100

    FEAT -> Created profileCard and profileText to represent the user's profile || Created the button to change between profileText and CInput to edit the profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,11 +1,84 @@
+import { useEffect, useState } from 'react';
+import { CInput } from "../../common/c-input/cInput";
+
 import './profile.css'
 import { Header } from "../../common/header/header";
+import { CButton } from "../../common/c-button/cButton";
 
 export const Profile = () => {
+
+    const [profileData, setProfileData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        passwordHash: ""
+    })
+
+    const [editable, setEditable] = useState("")
+
+    const editProfile = () => {
+        console.log(editable);
+        setEditable("yes")
+    }
+
+    const saveProfile = () => {
+        console.log(editable);
+        setEditable("")
+    }
+
     return (
         <>
             <Header />
-            <div className="profileDesign">Soy el profile</div>
+            <div className="profileDesign">
+                {editable === ""
+                    ? (
+                        <div className="profileCard">
+                            <div className="profileImg"></div>
+                            <div className="profileTextBox">
+                                <div className="profileText"></div>
+                                <div className="profileText"></div>
+                                <div className="profileText"></div>
+                                <div className="profileText"></div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="profileCard">
+                            <div className="profileImg"></div>
+                            <div className="profileTextBox">
+                                <CInput
+                                    className={"inputDesign"}
+                                    type={"text"}
+                                    name={"firstName"}
+                                    value={""}
+                                    placeholder={"input name"}
+                                />
+                                <CInput
+                                    className={"inputDesign"}
+                                    type={"text"}
+                                    name={"firstName"}
+                                    value={""}
+                                    placeholder={"input name"}
+                                />
+                                <CInput
+                                    className={"inputDesign"}
+                                    type={"text"}
+                                    name={"firstName"}
+                                    value={""}
+                                    placeholder={"input name"}
+                                />
+                                <CInput
+                                    className={"inputDesign"}
+                                    type={"text"}
+                                    name={"firstName"}
+                                    value={""}
+                                    placeholder={"input name"}
+                                />
+                            </div>
+                        </div>
+                    )}
+                <CButton onClick={editable === "" ? editProfile : saveProfile} title={editable === "" ? "Edit profile" : "Save changes"} />
+
+            </div>
         </>
     )
 }
