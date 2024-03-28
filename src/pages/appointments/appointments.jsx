f053b01ef9d8555b3bb6ac20155e754df211bd49
JesusMartinezClavel <jmcvalles@gmail.com>
2024-03-28 18:58:23 +0100

    FEAT -> Created getOwnAppointments function, the appointments view and its hooks

--- src/pages/appointments/appointments.jsx
+++ src/pages/appointments/appointments.jsx
@@ -1,16 +1,47 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import './appointments.css'
 import { Header } from "../../common/header/header";
+import { getOwnAppointments } from "../../services/apiCalls";
+import { CCard } from "../../common/c-card/cCard";
+import { CText } from "../../common/c-text/cText";
 
 export const Appointments = () => {
 
+    const tokenData = JSON.parse(localStorage.getItem("passport"))
+    const navigate = useNavigate()
+
     const [appointments, setAppointments] = useState([])
+    const [storagedToken, setStoragedToken] = useState(tokenData?.userToken)
+
+    useEffect(() => {
+        const getAppointments = async () => {
+            try {
+                const fetched = await getOwnAppointments(storagedToken)
+                setAppointments(fetched.data)
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        getAppointments()
+    }, [])
+
+    console.log(appointments);
 
     return (
         <>
             <Header />
-            <div className="appointmentsDesign"></div>
+            <div className="appointmentsDesign">
+                {appointments.map((appointments, index)=>(
+                    <CCard key={index} className={"cardDesign"}>
+                        <CText>
+                            <div className="appointmentDate">{appointments.appointmentDate}</div>
+                            <div className="service">{appointments.service}</div>
+                        </CText>
+                    </CCard>
+                ))}
+            </div>
         </>
     )
 }
